d72b36a9512d322021715c9fcdedcaf71fdb3c92
yogaboll <markus.s.englund@gmail.com>
2017-08-03 18:23:35 +0700

    proj-structure: add folders for routelist and routeInput

--- src/components/RouteInput/index.jsx
+++ src/components/RouteInput/index.jsx
@@ -1,24 +1,20 @@
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import InputModeToggle from "./InputModeToggle"
 import AdvancedInput from "./AdvancedInput"
 import SearchInput from "./SearchInput"
 
-
-class RouteInput extends Component {
-  render() {
-    const { inputMode } = this.props
-    return (
-      <div>
-        { inputMode === "search" ?
-          <SearchInput /> :
-          <AdvancedInput />
-        }
-        <InputModeToggle inputMode={inputMode} />
-      </div>
-    )
-  }
+function RouteInput({ inputMode }) {
+  return (
+    <div>
+      { inputMode === "search" ?
+        <SearchInput /> :
+        <AdvancedInput />
+      }
+      <InputModeToggle inputMode={inputMode} />
+    </div>
+  )
 }
 
 RouteInput.propTypes = {
