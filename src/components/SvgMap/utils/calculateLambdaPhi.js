7e75b34f7c2643a0241ca5755f74404a4fe18091
yogaboll <markus.s.englund@gmail.com>
2017-08-23 23:17:28 +0700

    split up svg-map component

---
+++ src/components/SvgMap/utils/calculateLambdaPhi.js
@@ -0,0 +1,28 @@
+import { geoBounds } from "d3-geo"
+
+export default function calculateLambdaPhi(sectors) {
+  let lambda = 0
+  let phi = 0
+  if (sectors.length) {
+    const lineStringCoords = sectors.map((sector) => {
+      return [[sector[0].lng, sector[0].lat], [sector[1].lng, sector[1].lat]]
+    })
+    const multiLineString = { type: "MultiLineString", coordinates: lineStringCoords }
+    const boundingBox = geoBounds(multiLineString)
+
+    if (boundingBox[0][0] <= boundingBox[1][0]) {
+      lambda = -(boundingBox[0][0] + boundingBox[1][0]) / 2
+    } else {
+      lambda = (-(boundingBox[0][0] + boundingBox[1][0] + 360) / 2)
+    }
+
+    if (phi < -65) {
+      phi = -65
+    } else if (phi > 65) {
+      phi = 65
+    } else {
+      phi = -(boundingBox[0][1] + boundingBox[1][1]) / 2
+    }
+  }
+  return { lambda, phi }
+}
