b8612040892d742c2a7be4893d61884021b69ed9
yogaboll <markus.s.englund@gmail.com>
2017-09-11 16:12:10 +0700

    add eslint npm script

--- src/reducers/airportData.js
+++ src/reducers/airportData.js
@@ -2,7 +2,6 @@ const airportData = (state = [], action) => {
   switch (action.type) {
     case "RECEIVE_AIRPORT_DATA":
       if (state.length > 0) {
-        console.log("CODE RED: AIRPORT DATA LOADED TWICE SOMETHING IS VERY WRONG!!!!!")
         throw new Error("AIRPORT DATA LOADED TWICE")
       }
       return action.data
