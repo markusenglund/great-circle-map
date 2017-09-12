a068e3802d9fa7f70eac86c5dca3decb9af5cf1e
yogaboll <markus.s.englund@gmail.com>
2017-09-12 16:27:49 +0700

    stage for production

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
 import ReduxThunk from "redux-thunk"
-import logger from "redux-logger"
+// import logger from "redux-logger"
 /* eslint-disable */
-import Perf from "react-addons-perf"
+// import Perf from "react-addons-perf"
 /* eslint-enable */
 
 import "./stylesheets/styles.scss"
@@ -18,10 +18,10 @@ import reducer from "./reducers"
 import App from "./components/App"
 
 // Remove in production
-window.Perf = Perf
+// window.Perf = Perf
 
 
-const store = createStore(reducer, applyMiddleware(ReduxThunk, logger))
+const store = createStore(reducer, applyMiddleware(ReduxThunk))
 
 ReactDOM.render(
   <Provider store={store}>
