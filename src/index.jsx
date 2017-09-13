554f6153e2483481b0edd342f09b345f7113f570
yogaboll <markus.s.englund@gmail.com>
2017-09-13 13:42:49 +0700

    enable analytics for react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import "babel-polyfill"
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Router, Route } from "react-router-dom"
 import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
 import ReduxThunk from "redux-thunk"
+import createBrowserHistory from "history/createBrowserHistory"
 // import logger from "redux-logger"
 /* eslint-disable */
 // import Perf from "react-addons-perf"
@@ -20,12 +21,32 @@ import App from "./components/App"
 // Remove in production
 // window.Perf = Perf
 
+const history = createBrowserHistory()
+
+const initializeAnalytics = () => {
+  /* eslint-disable */
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-104156354-1', 'auto');
+  ga('send', 'pageview');
+  /* eslint-enable */
+
+  history.listen((location) => {
+    console.log(`tracking page view: ${location.pathname}`)
+    ga("send", "pageview", location.pathname)
+  })
+}
+
+initializeAnalytics()
 
 const store = createStore(reducer, applyMiddleware(ReduxThunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <Route exact path="/:string?" component={App} />
     </Router>
   </Provider>,
