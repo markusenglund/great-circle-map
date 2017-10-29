f1499ed69c963e2897c9bea60302d8f68402a4c7
yogaboll <markus.s.englund@gmail.com>
2017-10-29 22:37:59 +0700

    make error page more user friendly

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'redux-little-router';
 
 export default function Error404() {
-  return <h2>Error 404</h2>;
+  return (
+    <div className="error-page">
+      <h2>Error 404 - page not found</h2>
+      <p>
+        <Link className="error-link" href="/">
+          Click here
+        </Link>{' '}
+        to get back to the main page.
+      </p>
+    </div>
+  );
 }
