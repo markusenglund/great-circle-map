9d059299070a0913ea8b135802a349170c3cbe97
yogaboll <markus.s.englund@gmail.com>
2017-10-30 15:22:45 +0700

    remove logs

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,4 +1,5 @@
 import getRoutes from './getRoutes';
+import { getAirports, getSectors, getGlobePosition } from './routeSelectors';
 import { getBrighterColor } from './colorSelector';
 
-export { getRoutes, getBrighterColor };
+export { getRoutes, getAirports, getSectors, getGlobePosition, getBrighterColor };
