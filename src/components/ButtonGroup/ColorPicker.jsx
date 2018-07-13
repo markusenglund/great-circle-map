c60d89f2fa889f48f7ce8b27c30cd76effa395d8
Markus Englund <markus.s.englund@gmail.com>
2018-07-14 00:30:27 +0200

    remove react-color

--- src/components/ButtonGroup/ColorPicker.jsx
+++ src/components/ButtonGroup/ColorPicker.jsx
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import onClickOutside from 'react-onclickoutside';
-import { GithubPicker } from 'react-color';
+
+const colors = [
+  '#D03030',
+  '#B80000',
+  '#DB3E00',
+  '#FCCB00',
+  '#008B02',
+  '#12DEDE',
+  '#004DCF',
+  '#5300EB',
+  '#EB9694',
+  '#FAD0C3',
+  '#FEF3BD',
+  '#F1D175',
+  '#8EFAAC',
+  '#A4BED6',
+  '#8EA3F3',
+  '#111111'
+];
 
 class ColorPicker extends Component {
   handleClickOutside() {
@@ -13,29 +31,14 @@ class ColorPicker extends Component {
     const { color, handleChange } = this.props;
     return (
       <div className="color-picker">
-        <GithubPicker
-          onChange={handleChange}
-          triangle="top-right"
-          color={color}
-          colors={[
-            '#D03030',
-            '#B80000',
-            '#DB3E00',
-            '#FCCB00',
-            '#008B02',
-            '#12DEDE',
-            '#004DCF',
-            '#5300EB',
-            '#EB9694',
-            '#FAD0C3',
-            '#FEF3BD',
-            '#F1D175',
-            '#8EFAAC',
-            '#A4BED6',
-            '#8EA3F3',
-            '#111111'
-          ]}
-        />
+        {colors.map(color => (
+          <button
+            key={color}
+            style={{ background: color }}
+            className="color-picker-color"
+            onClick={() => handleChange({ hex: color })}
+          />
+        ))}
       </div>
     );
   }
